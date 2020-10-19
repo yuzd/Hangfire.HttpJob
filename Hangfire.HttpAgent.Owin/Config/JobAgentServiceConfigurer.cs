ce0f60248bad82895521b70fa1079309166f34e2
朱坚 <850232649@qq.com>
2020-10-19 11:48:40 +0800

    add owin

---
+++ Hangfire.HttpAgent.Owin/Config/JobAgentServiceConfigurer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Reflection;
+using Hangfire.HttpAgent.Owin.Attribute;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hangfire.HttpAgent.Owin.Config
+{
+
+
+    public class JobAgentServiceConfigurer
+    {
+
+        internal static readonly ConcurrentDictionary<Type, JobMetaData> JobAgentDic = new ConcurrentDictionary<Type, JobMetaData>();
+
+        public IServiceCollection Services { get; }
+
+        public JobAgentServiceConfigurer(IServiceCollection serviceCollection)
+        {
+            Services = serviceCollection;
+        }
+
+        /// <summary>
+        /// 这种类型的job 可以并发运行
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public JobAgentServiceConfigurer AddTransientJobAgent<T>() where T : JobAgent
+        {
+            var type = typeof(T);
+            if (JobAgentDic.ContainsKey(type)) return this;
+            var meta = new JobMetaData
+            {
+                Transien = true
+            };
+            var hangJobUntilStopAttribute = type.GetCustomAttribute<HangJobUntilStopAttribute>();
+            if (hangJobUntilStopAttribute != null)
+            {
+                meta.Hang = hangJobUntilStopAttribute.On;
+            }
+            if (JobAgentDic.TryAdd(type, meta))
+            {
+                Services.AddTransient<T>();
+            }
+            return this;
+        }
+        public JobAgentServiceConfigurer AddTransientJobAgent(Type type)
+        {
+            if (!typeof(JobAgent).IsAssignableFrom(type))
+            {
+                throw new InvalidCastException($"type:{type.Name} is not AssignableFrom typeOf JobAgent");
+            }
+            if (JobAgentDic.ContainsKey(type)) return this;
+
+            var meta = new JobMetaData
+            {
+                Transien = true
+            };
+            var hangJobUntilStopAttribute = type.GetCustomAttribute<HangJobUntilStopAttribute>();
+            if (hangJobUntilStopAttribute != null)
+            {
+                meta.Hang = hangJobUntilStopAttribute.On;
+            }
+            if (JobAgentDic.TryAdd(type, meta))
+            {
+                Services.AddTransient(type);
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// 添加一次 只能运行一次 运行结束才能运行下一次
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public JobAgentServiceConfigurer AddJobAgent<T>() where T : JobAgent
+        {
+            var type = typeof(T);
+            AddJobAgent(type);
+            return this;
+        }
+
+        public JobAgentServiceConfigurer AddJobAgent(Type type)
+        {
+            if (!typeof(JobAgent).IsAssignableFrom(type))
+            {
+                throw new InvalidCastException($"type:{type.Name} is not AssignableFrom typeOf JobAgent");
+            }
+            if (JobAgentDic.ContainsKey(type)) return this;
+
+            var scopedJobAttribute = type.GetCustomAttribute<TransientJobAttribute>();
+
+
+            var meta = new JobMetaData();
+
+            var hangJobUntilStopAttribute = type.GetCustomAttribute<HangJobUntilStopAttribute>();
+            if (hangJobUntilStopAttribute != null)
+            {
+                meta.Hang = hangJobUntilStopAttribute.On;
+            }
+
+            if (scopedJobAttribute != null)
+            {
+                meta.Transien = true;
+                if (JobAgentDic.TryAdd(type, meta))
+                {
+                    Services.AddTransient(type);
+                }
+                return this;
+            }
+            meta.Transien = false;
+            if (JobAgentDic.TryAdd(type, meta))
+            {
+                Services.AddSingleton(type);
+            }
+            return this;
+        }
+
+        public JobAgentServiceConfigurer AddJobAgent(Assembly assembly) 
+        {
+            var types = assembly.GetExportedTypes();
+            var agengList = (from t in types
+                where typeof(JobAgent).IsAssignableFrom(t) &&
+                      !t.IsAbstract &&
+                      !t.IsInterface
+                select t).ToList();
+            agengList.ForEach(r=>AddJobAgent(r));
+            return this;
+        }
+    }
+}
---
+++ Hangfire.HttpAgent.RedisConsole.Owin/JobAgentConsoleCollectionExtensions.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using System;
+using Hangfire.HttpAgent.Owin.Config;
+using Hangfire.HttpAgent.RedisConsole.Owin.Config;
+using Hangfire.HttpAgent.RedisConsole.Owin;
+using Hangfire.HttpAgent.Owin;
+using Microsoft.Extensions.Logging;
+using Owin;
+using System.Linq;
+
+namespace Hangfire.HttpAgent.RedisConsole.Owin
+{
+    public static class JobAgentConsoleCollectionExtensions
+    {
+
+        public static IServiceCollection AddOwinHangfireJobAgent(this IServiceCollection services, Action<JobAgentServiceConfigurer> configure = null)
+        {
+            services.AddHangfireHttpJobAgent(configure);
+            services.AddJobAgentConsoleToRedis();
+            return services;
+        }
+
+
+        public static IAppBuilder UseOwinHangfireJobAgent(this IAppBuilder app, IServiceCollection services,
+          Action<JobAgentOptionsConfigurer> configureOptions = null, Action<RedisConsoleOptionsConfigurer> configureStorageOptions = null)
+        {
+            app.UseHangfireHttpJobAgent(services, configureOptions);
+            app.UseJobAgentConsoleToRedis(services,configureStorageOptions);
+            return app;
+        }
+
+        public static IServiceCollection AddHangfireJobAgent(this IServiceCollection services, Action<JobAgentServiceConfigurer> configure = null)
+        {
+            services.AddHangfireHttpJobAgent(configure);
+            services.AddJobAgentConsoleToRedis();
+            return services;
+        }
+
+
+        public static IServiceCollection AddJobAgentConsoleToRedis(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddOptions();
+            serviceCollection.TryAddSingleton<IConfigureOptions<RedisStorageOptions>, RedisConsoleOptions>();
+            serviceCollection.TryAddSingleton<IHangfireStorage, RedisStorage>();
+            serviceCollection.TryAddTransient<IHangfireConsole, RedisConsole>();
+            serviceCollection.TryAddTransient<IStorageFactory, IRedisStorageFactory>();
+            return serviceCollection;
+        }
+
+
+
+        public static IAppBuilder UseJobAgentConsoleToRedis(this IAppBuilder app, IServiceCollection services, Action<RedisConsoleOptionsConfigurer> configureOptions = null)
+        {
+
+            var sp = services.BuildServiceProvider();
+
+            var evt = new EventId(1, "Hangfire.HttpJob.Agent.MysqlConsole");
+            var options = sp.GetService<IOptions<RedisStorageOptions>>();
+            var configurer = new RedisConsoleOptionsConfigurer(options.Value);
+            var loggerFactory = sp.GetService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger<RedisConsoleOptionsConfigurer>();
+            try
+            {
+                configureOptions?.Invoke(configurer);
+            }
+            catch (Exception exception)
+            {
+                logger.LogCritical(evt, exception, "【Hangfire.HttpAgent.RedisConsole.Owin】 - Failed to configure Hangfire.HttpAgent.RedisConsole.Owin middleware");
+            }
+
+            JobStorageConfig.LocalJobStorageConfig = new JobStorageConfig
+            {
+                Type = "redis",
+                HangfireDb = options.Value?.HangfireDb,
+                Db = options.Value?.DataBase,
+                ExpireAtDays = options.Value?.ExpireAtDays,
+                TablePrefix = options.Value?.TablePrefix
+            };
+
+            logger.LogInformation(evt, "【Hangfire.HttpAgent.RedisConsole.Owin】 - Registered RedisConsole middleware Success!");
+
+            return app;
+        }
+    }
+
+
+
+}
