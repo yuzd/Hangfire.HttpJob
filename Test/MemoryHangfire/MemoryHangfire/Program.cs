3e4d67aeb698c689286b95cc6e717c635f80f24a
yuzd <zdyu@Ctrip.com>
2020-11-28 15:12:11 +0800

    add  Hangfire.InMemory demo

---
+++ Test/MemoryHangfire/MemoryHangfire/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace MemoryHangfire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("NLog.Config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+        }
+        
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                        .ConfigureLogging(logging =>
+                        {
+                            logging.ClearProviders();
+#if DEBUG
+                            logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Debug);
+
+#else
+                            logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
+#endif
+                        }).UseNLog();
+                });
+    }
+}
