5ac269b1b1bae203826495ddce357b154189a49d
1877682825@qq.com <yuzd>
2021-01-16 20:54:11 +0800

    fix redis test project

--- Test/RedisHangfire/HangfireCollectionExtensions.cs
+++ Test/RedisHangfire/HangfireCollectionExtensions.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NLog.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Data;
